88dfdaca29fb95007603b5d208c9ca7e9ad79911
yatin166 <yatin16694@gmail.com>
2021-06-18 21:26:59 +0200

    setup react typescript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props { }
+
+interface State { }
+
+export class App extends React.Component<Props, State> {
+
+  render() {
+    return (
+      <div>
+        Dahsboard WIP
+      </div>
+    )
+  }
+}
