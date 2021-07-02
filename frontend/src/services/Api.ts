4a4decbc2bb562164df8e85c4d38cb50680501d4
yatin166 <yatin16694@gmail.com>
2021-07-02 14:41:52 +0200

    added login axios configuration

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,4 +1,7 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosError } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
+import { AccessTokenRequest } from '../dto/request/AccessToken.request';
+import { AccessTokenResponse } from '../dto/response/AccessToken.response';
+import { LocalStorage, LocalStorageKey } from '../storage/LocalStorage';
 
 export class Api {
 
@@ -8,19 +11,53 @@ export class Api {
     constructor(basePath: string) {
         this.basePath = basePath;
         this.http = axios.create()
-        this.http.interceptors.request.use(this.onSuccessRequest, this.onFailRequest)
+        this.http.interceptors.request.use(this.onRequestFulfilled, this.onRequestError);
+        this.http.interceptors.response.use(this.onResponseFulfilled, this.onResponseError)
     }
 
-    private onSuccessRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+    private onRequestFulfilled = (config: AxiosRequestConfig): AxiosRequestConfig => {
+        const token = LocalStorage.getAccessToken();
+        if (token) {
+            config.headers['Authorization'] = LocalStorage.getAccessToken();
+        }
         config.url = this.basePath + config.url;
         return config
     }
 
-    private onFailRequest = (error: AxiosError): Promise<AxiosError> => {
+    private onRequestError = (error: AxiosError): Promise<AxiosError> => {
         console.error({
             errorCode: error.code,
             message: error.message
         })
         return Promise.reject(error);
+    }
+
+    private onResponseFulfilled = (response: AxiosResponse): AxiosResponse => {
+        return response;
+    }
+
+    private onResponseError = (error: AxiosError) => {
+        const originalRequest = error.config;
+        if (error.response?.status === 401) {
+            const refreshToken = localStorage.getRefreshToken();
+            return this.getAccessToken('/access-token', refreshToken)
+                .then(response => {
+                    if (response.status === 200) {
+                        LocalStorage.persist(LocalStorageKey.ACCESS_TOKEN, response.data.accessToken);
+                        return axios(originalRequest);
+                    }
+                })
+                .catch(error => {
+                    console.error({
+                        errorCode: error.code,
+                        message: error.message
+                    })
+                    return Promise.reject(error);
+                });
+            }
+    }
+
+    public async getAccessToken(path: string, accessTokenRequest: AccessTokenRequest) {
+        return this.http.post<AccessTokenResponse>(path, accessTokenRequest);
     }
 }
--- frontend/src/storage/LocalStorage.ts
+++ frontend/src/storage/LocalStorage.ts
@@ -8,4 +8,12 @@ export class LocalStorage {
     public static persist(key: LocalStorageKey, data: any) {
         localStorage.setItem(key, data)
     }
+
+    public static getAccessToken(): string | null {
+        return localStorage.getItem(LocalStorageKey.ACCESS_TOKEN)
+    }
+
+    public static getRefreshToken(): string | null {
+        return localStorage.getItem(LocalStorageKey.REFRESH_TOKEN)
+    }
 }
