2e0512746944fa90c0d498539fe6c5bc38e65dff
yatin166 <yatin16694@gmail.com>
2021-07-13 21:10:08 +0200

    added dashboard scss

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Sidebar } from '../../components/sidebar/Sidebar';
+import styles from './Dashboard.module.scss'
 
 interface Props extends RouteComponentProps<{}> {}
 
 export const Dashboard: React.FunctionComponent<Props>  = (props: Props): JSX.Element => {
 
     return (
-        <div>
+        <div className={styles.dashboardContainer}>
             <Sidebar />
             Dashboard
         </div>
