8b05c005442e7d25eff4bdd65ee90dc49e682dc8
yatin166 <yatin16694@gmail.com>
2021-07-24 11:22:39 +0200

    added socket io in the server

--- microservices/dashboard/src/DashboardServer.ts
+++ microservices/dashboard/src/DashboardServer.ts
@@ -9,8 +9,8 @@ export class DashboardServer extends Database {
     private readonly expressApplication: express.Application;
     private readonly port: number;
     private readonly mainDashboardController: MainDashboardController;
-    private server: http.Server;
-    private io: socketIO.Server;
+    private readonly httpServer: http.Server;
+    private readonly socketIoServer: socketIO.Server;
 
     constructor(
         expressApplication: express.Application,
@@ -22,51 +22,12 @@ export class DashboardServer extends Database {
         this.expressApplication = expressApplication;
         this.port = port;
         this.mainDashboardController = mainDashboardController;
-        
 
-        this.expressApplication.use(bodyParser.json());
-
-        for (const route of this.mainDashboardController.routerConfiguration) {
-            this.expressApplication.use(route.path, route.controller.router);
-        }
-
-
-        this.server = new http.Server(this.expressApplication)
-        this.io = new socketIO.Server(this.server);
-
-
-
-        this.io.on('connection', (socket: socketIO.Socket) => {
-            console.log('a user connected : ' + socket.id)
-
-            socket.on("message", function(message: any) {
-                console.log(message);
-            });
-
-            socket.on('disconnect', function () {
-                console.log('socket disconnected : ' + socket.id);
-            });
-        })
-
-    }
-    public Start() {
-        this.server.listen(this.port)
-        console.log( `Server listening on port ${this.port}.` )
-
-        this.io.on('connection', (socket: socketIO.Socket) => {
-            console.log('a user connected : ' + socket.id)
-
-            socket.on("message", function(message: any) {
-                console.log(message);
-            });
-
-            socket.on('disconnect', function () {
-                console.log('socket disconnected : ' + socket.id);
-            });
-        })
+        this.httpServer = new http.Server(this.expressApplication)
+        this.socketIoServer = new socketIO.Server(this.httpServer, { cors: {  origin: '*' } } );
     }
 
-    /* public async configure(): Promise<void> {
+    public async configure(): Promise<void> {
         this.expressApplication.use(bodyParser.json());
 
         for (const route of this.mainDashboardController.routerConfiguration) {
@@ -77,21 +38,19 @@ export class DashboardServer extends Database {
     public up() {
         this.connect()
             .then(() => {
-                const socketIoServer = new socketIO.Server(
-                    this.expressApplication.listen(this.port, () => {
-                        console.log(`Database connection successful`)
-                        console.log(`Dashboard server is up and running on ${this.port}`)
-                    })
-                );
+                this.httpServer.listen(this.port, () => {
+                    console.log(`Database connection successful`)
+                    console.log(`Dashboard server is up and running on ${this.port}`)
+                });
 
-                socketIoServer.on('connection', (socket: socketIO.Socket) => {
+                this.socketIoServer.on('connection', (socket: socketIO.Socket) => {
                     console.log('a user connected : ' + socket.id)
         
                     socket.on('disconnect', function () {
                         console.log('socket disconnected : ' + socket.id);
                     });
-                })
+                });
             })
             .catch(error => console.log('error', error));
-    } */
+    }
 }
