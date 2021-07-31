5d7cee3442109c443a571a08013a323a804a7b33
yatin166 <yatin16694@gmail.com>
2021-07-31 16:07:30 +0200

    added socket data in state

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -12,24 +12,22 @@ interface Props extends RouteComponentProps<{}> {}
 
 export const Dashboard: React.FunctionComponent<Props>  = (props: Props): JSX.Element => {
 
-    const [socketData, setSocketData] = useState<any>();
+    const [socketData, setSocketData] = useState<number[]>([]);
 
-    useEffect(() => {
+    /* useEffect(() => {
         const ioServer = io('http://localhost:8001/');
-        setSocketData(ioServer);
-    }, [])
+        //setSocketData(ioServer);
+        ioServer.on('dataFromServer', (data) => {
+            console.log(data)
+        });
+    }, []) */
+    //console.log(socketData, 'socketData')
 
     const data = {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
         datasets: [
           {
-            data: [33, 53, 85, 41, 44, 65],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)"
-          },
-          {
-            data: [33, 25, 35, 51, 54, 76],
+            data: [...socketData],
             fill: false,
             borderColor: "#742774"
           }
@@ -43,12 +41,33 @@ export const Dashboard: React.FunctionComponent<Props>  = (props: Props): JSX.El
             }
         }
     }
+
+    const emitData = () => {
+        fetch('http://localhost:8001/api/v1/data/time-series')
+            .then((res) => res.json())
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+    }
+
+    const connect = () => {
+        const socketServer = io('http://localhost:8001/');
+        socketServer.on('dataForClient', message => {
+            //console.log(message, 'message')
+            //const lastFiveElements = socketData.splice(-5)
+            console.log(socketData, 'sockerData', message, 'msg')
+            setSocketData([...socketData, message]);
+        });
+    }
+    
     
     return (
         <div className={styles.dashboardContainer}>
             <Sidebar />
             
             <Flex.Horizontal>
+                <button onClick={connect}>Connect</button>
+                <button onClick={emitData}>Emit</button>
+
                 <div className={styles.chartContainer}>
                     <Line data={data} options={chartOptions}/>
                 </div>
