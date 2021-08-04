652a56978a35d2e12861ed72d4d0f532e9bc7a38
yatin166 <yatin16694@gmail.com>
2021-08-04 19:01:27 +0200

    refactored socket service

--- microservices/dashboard/src/SocketServer.ts
+++ microservices/dashboard/src/SocketServer.ts
@@ -1,22 +1,30 @@
-import socketIO from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export class SocketServer {
-    private readonly socketServer: socketIO.Server;
+    private readonly socketServer: Server;
 
-    constructor(socketServer: socketIO.Server) {
+    constructor(socketServer: Server) {
         this.socketServer = socketServer;
     }
 
     public enableEvents() {
-        this.socketServer.on('connection', (socket: socketIO.Socket) => {
+        this.socketServer.on('connection', (socket: Socket) => {
             console.log('New connection established: ' + socket.id)
+            this.emitDataForClient(socket);
+            this.disconnect(socket);          
+        });
+    }
 
-            socket.on('dataForServer', (data) => {
-                console.log('dataForServer', data)
-                socket.broadcast.emit('dataForClient', data);
-            });
+    private emitDataForClient(socket: Socket) {
+        socket.on('dataForServer', (data) => {
+            console.log('dataForServer', data)
+            socket.broadcast.emit('dataForClient', data);
+        });
+    }
 
-            socket.on('disconnect', () => console.log('Connection destroyed: ' + socket.id));
+    private disconnect(socket: Socket) {
+        socket.on('disconnect', () => {
+            console.log('Connection destroyed: ' + socket.id)
         });
     }
 }
