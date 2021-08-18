ea35835705e865efdd12c040f07ae1525a66ef90
yatin166 <yatin16694@gmail.com>
2021-08-18 20:42:48 +0200

    parsed refreshtoken json

--- frontend/src/components/routes/AuthRoute.tsx
+++ frontend/src/components/routes/AuthRoute.tsx
@@ -2,10 +2,21 @@ import {
     Route,
     Redirect
 } from 'react-router-dom';
+import { LocalStorage } from '../../storage/LocalStorage';
+import atob from 'atob';
 
 export const AuthRoute = ({ component: Component, ...rest }: any) => {
 
     const isAuthenticated: boolean = true;
+
+    const checkRefreshTokenValidity = () => {
+        const refreshToken = LocalStorage.getRefreshToken()
+        if (!refreshToken)
+            return
+
+        const refreshToeknPayload = JSON.parse(atob(refreshToken));
+        console.log(refreshToeknPayload, 'refreshToeknPayload')
+    }
 
     return (
         <Route
