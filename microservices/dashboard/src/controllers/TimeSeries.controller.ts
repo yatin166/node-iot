8965857d5bbe485d51fed9e24e82e64cde349744
yatin166 <yatin16694@gmail.com>
2021-09-11 02:13:02 +0200

    sample use of decorator

--- microservices/dashboard/src/controllers/TimeSeries.controller.ts
+++ microservices/dashboard/src/controllers/TimeSeries.controller.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { Request } from '../middleware/Request'
 import { authenticationMiddleware } from '../middleware/authentication.middleware';
 import { reqLoggerMiddleware } from '../middleware/reqLogger.middleware';
@@ -14,6 +14,66 @@ const Path = {
     Delete: '/delete',
     id: '/:id'
 }
+
+/* function logMethod(
+    target: Object,
+    propertyName: string,
+    propertyDesciptor: PropertyDescriptor
+): PropertyDescriptor {
+        
+    const method = propertyDesciptor.value;
+
+    propertyDesciptor.value = function (...args: any[]) {
+
+        // convert list of greet arguments to string
+        const params = args.map(a => JSON.stringify(a)).join();
+
+        // invoke greet() and get its return value
+        const result = method.apply(this, args);
+
+        // convert result to string
+        const r = JSON.stringify(result);
+
+        // display in console the function call details
+        console.log(`Call: ${propertyName}(${params}) => ${r}`);
+
+        // return the result of invoking the method
+        return result;
+    }
+    return propertyDesciptor;
+}; */
+
+function logMethod(
+    router: express.Router,
+    controller: any,
+    methodType: 'get' | 'post' | 'put' | 'delete',
+    path: string,
+    propertyDesciptor?: PropertyDescriptor
+) {
+
+    //return router.get(path, authenticationMiddleware, reqLoggerMiddleware, propertyDesciptor.value.bind(controller));
+    /* const method = propertyDesciptor.value;
+
+    propertyDesciptor.value = function (...args: any[]) {
+
+        // convert list of greet arguments to string
+        const params = args.map(a => JSON.stringify(a)).join();
+
+        // invoke greet() and get its return value
+        const result = method.apply(this, args);
+
+        // convert result to string
+        const r = JSON.stringify(result);
+
+        // display in console the function call details
+        console.log(`Call: ${propertyName}(${params}) => ${r}`);
+
+        // return the result of invoking the method
+        return result;
+    }
+    return propertyDesciptor; */
+};
+
 
 export class TimeSeriesController implements DashboardController {
     public readonly router: express.Router;
