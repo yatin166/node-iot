edf432806dbfd3835f30d37c2fe99918bad0b592
yatin166 <yatin16694@gmail.com>
2021-10-11 18:42:14 +0200

    reverted

--- microservices/dashboard/src/controllers/TimeSeries.controller.ts
+++ microservices/dashboard/src/controllers/TimeSeries.controller.ts
@@ -26,10 +26,14 @@ export class TimeSeriesController implements DashboardController {
     }
 
     private initRoutes() {
-        this.router.get(Path.All, authenticationMiddleware, this.getAllUsers.bind(this))
+        this.router.get(`${Path.TimeSeries}${Path.Emit}`, authenticationMiddleware, this.startEmitting.bind(this))
+        this.router.get(`${Path.TimeSeries}${Path.Stop}`, authenticationMiddleware, this.stopEmitting.bind(this))
+        this.router.get(`${Path.Socket}${Path.All}`, authenticationMiddleware, this.getSockets.bind(this))
+        this.router.get(`${Path.Socket}${Path.Id}`, authenticationMiddleware, this.getSocket.bind(this))
+        this.router.delete(`${Path.Socket}${Path.All}`, authenticationMiddleware, this.deleteSockets.bind(this))
+        this.router.delete(`${Path.Socket}${Path.Id}`, authenticationMiddleware, this.deleteSocket.bind(this))
     }
 
-    //@GET(`${Path.TimeSeries}${Path.Emit}`)
     async startEmitting(req: Request, res: express.Response, next: express.NextFunction) {
         if (!req.userId)
             return res.send({ message: 'Could not find userId in the request' });
@@ -38,8 +42,7 @@ export class TimeSeriesController implements DashboardController {
             .then(response => res.send(response))
             .catch(error => next(error))
     }
-
-    //@GET(`${Path.TimeSeries}${Path.Stop}`)
+    
     async stopEmitting(req: Request, res: express.Response, next: express.NextFunction) {
         if (!req.userId)
             return res.send({ message: 'Could not find userId in the request' });
@@ -49,29 +52,25 @@ export class TimeSeriesController implements DashboardController {
             .catch(error => next(error))
     }
 
-    @GET(`${Path.Socket}${Path.All}`)
     async getSockets(req: Request, res: express.Response, next: express.NextFunction) {
         console.log('in socket controller')
         this.socketService.getSockets()
             .then(sockets => res.send(sockets))
             .catch(error => next(error));
     }
-
-    //@GET(`${Path.Socket}${Path.Id}`)
+    
     async getSocket(req: Request, res: express.Response, next: express.NextFunction) {
         this.socketService.getSocket(req.params.id)
             .then(socket => res.send(socket))
             .catch(error => next(error));
     }
-
-    //@DELETE(`${Path.Socket}${Path.All}`)
+    
     async deleteSockets(req: Request, res: express.Response, next: express.NextFunction) {
         this.socketService.deleteSockets()
             .then(() => res.send({ message: 'All sockets are deleted'}))
             .catch(error => next(error));
     }
-
-    //@DELETE(`${Path.Socket}${Path.Id}`)
+    
     async deleteSocket(req: Request, res: express.Response, next: express.NextFunction) {
         const id: string = req.params.id;
         this.socketService.deleteSocket(id)
