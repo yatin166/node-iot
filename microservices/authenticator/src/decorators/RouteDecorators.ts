84917ac55da4d88dcb6a209020d585899aec299d
yatin166 <yatin16694@gmail.com>
2021-10-13 23:49:38 +0200

    removed comments

--- microservices/authenticator/src/decorators/RouteDecorators.ts
+++ microservices/authenticator/src/decorators/RouteDecorators.ts
@@ -12,16 +12,6 @@ const routeDecorator = (method: Method, path: string) => {
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) => {
-        //(router)[method](path, target[propertyKey]).bind(target);
-        //(router)[method](path, response);
-        /*console.log("target ", target)
-        console.log("propertyKey ", propertyKey)
-        console.log("target[propertyKey] ", target[propertyKey].toString())
-        console.log("descriptor.value ", descriptor.value);
-        target.constructor.bind(target[propertyKey]);
-        (router)[method](path, target[propertyKey]);*/
-        //console.log("target.constructor.name", target.constructor.name)
-
         (router)[method](path, target[propertyKey]).bind(target.constructor);
         return
     }
