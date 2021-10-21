7c2cff2e08cc94c62674bd9ca388466733ae5e70
yatin166 <yatin16694@gmail.com>
2021-10-21 17:21:49 +0200

    added in server

--- microservices/authenticator/src/server.ts
+++ microservices/authenticator/src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express, { Router } from 'express'
 import cors from 'cors';
 import { MainAuthenticationController } from './controllers/base/Main.authentication.controller';
 import bodyParser from 'body-parser'
 import { Database } from './database/Database';
-import { Metadata } from './decorators/RouteDecorators';
+import { Metadata, RouteDefinition } from './decorators/RouteDecorators';
 import 'reflect-metadata';
 import { AuthenticationController } from './controllers/Authentication.controller';
 
@@ -28,11 +28,22 @@ export class Server extends Database {
         this.expressApplication.use(cors())
         this.expressApplication.use(bodyParser.json());
 
+        const ro = Router();
+
         for (const route of this.mainAuthenticationController.routerConfiguration) {
             this.expressApplication.use(route.path, route.controller.router)
             console.log('constructor name ', route.controller.constructor.name)
             console.log('metadata ', Reflect.getMetadata(Metadata.ROUUTES, route.controller.constructor))
+
+            const routes: Array<RouteDefinition> = Reflect.getMetadata(Metadata.ROUUTES, route.controller.constructor);
+    
+            routes.forEach(route2 => {
+                ro[route2.requestMethod](route2.path, (req: express.Request, res: express.Response) => {
+                  route.controller[route2.methodName](req, res);
+                });
+            });
         }
+
     }
 
     public up() {
