98846c82ccc196bfedaf7bddeaf583556ca6800c
yatin166 <yatin16694@gmail.com>
2021-10-22 20:34:01 +0200

    deleted test controller

--- microservices/authenticator/src/controllers/TestController.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { GET } from '../decorators/RouteDecorators';
-
-export class TestController {
-
-    login(req: express.Request, res: express.Response, next: express.NextFunction) {
-        res.send({message: "Hello World!"})
-    }
-}
