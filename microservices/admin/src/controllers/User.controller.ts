8d02c40b2e7a07aab78e0ce660e80738011931d0
yatin166 <yatin16694@gmail.com>
2021-10-24 19:23:08 +0200

    cleanup admin serverce for decorators

--- microservices/admin/src/controllers/User.controller.ts
+++ microservices/admin/src/controllers/User.controller.ts
@@ -7,16 +7,6 @@ const Path = {
 }
 
 export class UserController implements AdminController {
-    public readonly router: express.Router;
-
-    constructor(router: express.Router) {
-        this.router = router;
-        this.initRoutes()
-    }
-
-    private initRoutes() {
-        this.router.get(Path.All, authenticationMiddleware, this.getAllUsers.bind(this))
-    }
 
     async getAllUsers(req: express.Request, res: express.Response, next: express.NextFunction) {
         //const users = await UserRepository.getAll();
