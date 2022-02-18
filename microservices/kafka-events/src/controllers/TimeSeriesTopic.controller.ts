fbec3b0a1b4ab832d4fa19ab8976abdeb5fdf972
yatin166 <yatin16694@gmail.com>
2022-02-19 00:24:45 +0100

    added kafka service in the setup to be consumed by controller

--- microservices/kafka-events/src/controllers/TimeSeriesTopic.controller.ts
+++ microservices/kafka-events/src/controllers/TimeSeriesTopic.controller.ts
@@ -2,23 +2,28 @@ import express from 'express';
 import { Request } from '../../../common/middlewares/Request';
 import { KafkaController } from './base/Main.kafka.controller';
 import { POST } from '../../../common/decorators/RouteDecorators';
+import { KafkaService } from '../services/Kafka.service';
 
 export class TimeSeriesTopicController implements KafkaController {
 
-    constructor() { }
+    private readonly kafkaService: KafkaService;
+
+    constructor(kafkaService: KafkaService) { 
+        this.kafkaService = kafkaService;
+    }
 
     @POST('/produce')
     async produceEvents(req: Request, res: express.Response, next: express.NextFunction) {
         if (!req.userId)
             return res.send({ message: 'Could not find userId in the request' });
-        console.log("Shall start producing the events")
+        this.kafkaService.produce(req.userId);
     }
     
     @POST('/consume')
     async consumeEvents(req: Request, res: express.Response, next: express.NextFunction) {
         if (!req.userId)
             return res.send({ message: 'Could not find userId in the request' });
-        console.log("Shall start consuming the events")
+        this.kafkaService.produce(req.userId);
     }
 }
 
