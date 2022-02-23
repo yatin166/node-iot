663ec01abd7e52b138e5875eff3b1b5362819207
yatin166 <yatin16694@gmail.com>
2022-02-23 19:14:16 +0100

    new side bar implementation

--- frontend/src/components/sidebar/NewSidebar.tsx
+++ frontend/src/components/sidebar/NewSidebar.tsx
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { Flex } from '../../components/container/flex/Flex'
 import { Card } from '../../components/card/Card';
 import { Path } from '../../components/routes/Path';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import { styled, Theme, CSSObject } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
-import LogoutIcon from '@mui/icons-material/Logout';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
 import ListItem from '@mui/material/ListItem';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Button, CssBaseline, Divider, IconButton } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { Header } from '../header/Header';
 
 interface Props extends RouteComponentProps<{}> {}
@@ -28,91 +19,85 @@ interface Props extends RouteComponentProps<{}> {}
 const drawerWidth = 240;
 
 const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(9)} + 1px)`,
-  },
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(9)} + 1px)`,
+    },
 });
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
     width: drawerWidth,
     flexShrink: 0,
     whiteSpace: 'nowrap',
     boxSizing: 'border-box',
     ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
     }),
     ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
     }),
-  }),
-);
+}));
 
 const NewSidebar: React.FunctionComponent<Props>  = ({ history }): JSX.Element => {
 
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+    const [toggle, setOpen] = React.useState(false);
 
-    const toggleDrawer = () => {
-        setOpen(!open);
-    };
+    const toggleDrawer = () => setOpen(!toggle);
 
     return (
         <Box sx={{ display: 'flex' }}>
           <CssBaseline />
           <Header drawerWidth={drawerWidth} toggleDrawer={toggleDrawer}/>
-          <Drawer variant="permanent" open={open}>
+          <Drawer variant="permanent" open={toggle}>
             <DrawerHeader />
             <List>
                 <ListItem button key={'Dashboard'}>
-                  <ListItemIcon>
-                    <HomeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={'Dashboard'} />
+                    <ListItemIcon>
+                      <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'Dashboard'} />
                 </ListItem>
             </List>
           </Drawer>
           <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            <DrawerHeader />
-                <Flex.Horizontal>
-                    <Card onClick={() => history.push(`/${Path.DASHBOARD}/${Path.CHARTS}/${Path.TIME_SERIES_CHART}`)}>
-                        <div >
-                            Time series chart
-                        </div>
-                    </Card>
+              <DrawerHeader />
+              <Flex.Horizontal>
+                  <Card onClick={() => history.push(`/${Path.DASHBOARD}/${Path.CHARTS}/${Path.TIME_SERIES_CHART}`)}>
+                      <div >
+                          Time series chart
+                      </div>
+                  </Card>
 
-                    <Card onClick={() => history.push(`/${Path.DASHBOARD}/${Path.CHARTS}/${Path.SCATTER_CHART}`)}>
-                        <div>
-                            Scatter chart
-                        </div>
-                    </Card>
-                </Flex.Horizontal>
+                  <Card onClick={() => history.push(`/${Path.DASHBOARD}/${Path.CHARTS}/${Path.SCATTER_CHART}`)}>
+                      <div>
+                          Scatter chart
+                      </div>
+                  </Card>
+              </Flex.Horizontal>
           </Box>
         </Box>
       );
